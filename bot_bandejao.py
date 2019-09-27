b5c060de1d73cbf5dc6bb1d750e45eea326c9175
Vitor  Mafra <vitor.mafra@hotmail.com>
2019-09-27 16:57:29 -0300

    Changed to ChromeDriver, remake autopost
    With ChromeDriver the script can automatically run in EC2 without problems (this part was remaked using cron jobs on the AWS).  This way i hope the costs get strongly lower and make it viable to run daily only with the AWS students' credits

--- bot_bandejao.py
+++ bot_bandejao.py
@@ -1,37 +1,46 @@
 import tweepy
-from bs4 import BeautifulSoup
+#from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys #apenas pra facilitar a leitura do codigo
+from selenium.webdriver.chrome.options import Options # apenas pra facilitar a leitura do codigo
+from selenium.webdriver.common.keys import Keys
 import datetime
 import time
 
 
-#"constantes" - bem, talvez nao exatamente
-    #chaves geradas pelo app do twitter
+# Essa parte inicial traz "constantes" e informacoes usadas ao longo do codigo
+
+# chaves geradas pelo Twitter para postagens no seu perfil (de desenvolvedor)
 CONSUMER_KEY = '**********************'
 CONSUMER_SECRET = '********************************************'
 ACCESS_KEY = '**********************-**********************'
 ACCESS_SECRET = '********************************************'
-    #paginas web necessarias para buscar o cardapio
+
+# paginas web necessarias para buscar o cardapio
 site_fump = "http://www.fump.ufmg.br/cardapio.aspx"
-    #restaurantes
+
+# restaurantes
 RU_SETORIAL_I = 1
 RU_SETORIAL_II = 2
 RU_SAUDE_E_DIREITO = 3
 RU_ICA = 4
 restaurantes_almoco_seg_sex = (RU_SETORIAL_I, RU_SETORIAL_II, RU_SAUDE_E_DIREITO, RU_ICA)
 restaurantes_almoco_sab = (RU_SETORIAL_I, RU_SAUDE_E_DIREITO, RU_ICA)
-restaurantes_jantar_seg_sab = (RU_SETORIAL_I, RU_SAUDE_E_DIREITO)
-#restaurantes_jantar_seg_sab = (RU_ICA, RU_ICA)
+restaurantes_jantar_seg_sab = (RU_SETORIAL_I, RU_SAUDE_E_DIREITO, RU_ICA)
 
-    # variaveis relacionandas ao tempo
-almoco = False
-jantar = True
+# variaveis relacionandas ao tempo
+diferenca = datetime.timedelta(hours=-3) #nao consegui instalar o pytz na AWS, tenho que arrumar isso
+fuso_horario = datetime.timezone(diferenca)
+
 dia_da_semana = datetime.datetime.today().weekday() # Segunda = 0, Terca = 1 ... Domingo = 6
 
-#setting webbrowser
-browser = webdriver.Firefox()   
-    
+almoco = False
+jantar = False
+
+# setting web browser
+options = Options()
+options.headless = True # o navegador passa a nao ter interface grafica (ja' que estara' rodando em um EC2)
+browser = webdriver.Chrome(options = options)   
+
 #setting API (https://tweepy.readthedocs.io/en/latest/getting_started.html#hello-tweepy)
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
@@ -39,21 +48,25 @@ auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
 
+# Ja essa parte traz funcoes que serao usadas para acessar o site da fump, pegar o cardapio, trata-lo e por fim, posta-lo no Twitter
+
 def pega_cardapio(site_fump, restaurante, almoco, jantar):
-    #tentando abrir o site da fump
+
+    # tentando abrir o site da Fump
     acessa_site_fump(site_fump)
 
-    #tentando selecionar o restaurante da caixa de selecao
+    # o cardapio para todos os restaurantes fica na mesma pagina no site da Fump. Alguma coisa em JS acontece por tras de uma
+    # caixa de selecao de restaurantes e, somente a partir dai, o conteudo dos cardapios pode ser acessado
     seleciona_caixa_de_selecao(restaurante)
 
-    #procurando os itens cardapio
-    cardapio = encontra_cardapio(almoco, jantar)
-        
+    # procurando e retornando os itens do cardapio desejado, i.e., almoco ou jantar (afinal, o restaurante ja foi escolhido)
+    cardapio = encontra_cardapio(almoco, jantar)        
     return cardapio
 
 
 def acessa_site_fump(site_fump):
-    #tentando abrir o site da fump
+
+    # tentando abrir o site da Fump
     try:
         browser.get(site_fump)
     except:
@@ -61,16 +74,17 @@ def acessa_site_fump(site_fump):
 
 
 def seleciona_caixa_de_selecao(restaurante):
-    caixa_de_selecao = browser.find_element_by_name("ctl00$contentPlaceHolder$drpRestaurante") #nome da caixa de selecao no site
+
+    caixa_de_selecao = browser.find_element_by_name("ctl00$contentPlaceHolder$drpRestaurante") # nome da caixa de selecao no site
     
     try:        
         if restaurante == RU_SETORIAL_I:
             caixa_de_selecao.click() 
             caixa_de_selecao.send_keys('r')             # Por algum motivo desconhecido, tentar enviar a tecla DOWN mais de uma
-            caixa_de_selecao.send_keys(Keys.ENTER)      # vez nao estava funcionando. Nao sei se essa eh uma limitacao ou bug da
+            caixa_de_selecao.send_keys(Keys.ENTER)      # vez nao estava funcionando. Nao sei se essa e' uma limitacao ou bug da
                                                         # biblioteca, ou se tem algo a ver com o site. Tentei navegar por esse 
         elif restaurante == RU_SETORIAL_II:             # select de todas as formas obvias e fazendo algumas variacoes. Por sorte
-            caixa_de_selecao.click()                    # descobri que apertar a tecla R navegava pelo menu. Eh exatamente isso que
+            caixa_de_selecao.click()                    # descobri que apertar a tecla R navegava pelo menu. E' exatamente isso que
             caixa_de_selecao.send_keys('r', 'r')        # esta sendo feito aqui. Como todas as opcoes de restaurantes universitarios
             caixa_de_selecao.send_keys(Keys.ENTER)      # comecam com "RU", apertar a tecla R navega por cada uma dessas opcoes - da 
                                                         # primeira `a ultima. Muito obrigado ao @gnus e ao @jottagpg por terem me 
@@ -138,13 +152,15 @@ def confere_cardapio(cardapio):
     count_caracteres = 0
     for letra in cardapio:
         count_caracteres += 1
-    #confere se ele eh tuitavel, ie, tem menos de 280 caracteres
+    #confere se ele e' tuitavel, ie, tem menos de 280 caracteres
     if (count_caracteres <= 280):
         mais_280_caracteres = False
     else:
         mais_280_caracteres = True
 
-    return mais_280_caracteres # a ideia eh definir outros tipos de possiveis erros aqui nessa funcao e retorna-los todos juntos
+    return mais_280_caracteres
+
+    #a ideia e' definir outros tipos de possiveis erros aqui nessa funcao e retorna-los todos juntos
 '''
 
 #tweetando (ou ao menos tentando)
@@ -166,10 +182,11 @@ def faz_tweet(restaurante, cardapio):
     count_caracteres = 0
     for letra in tweet:
         count_caracteres += 1
-    #confere se ele eh tuitavel, ie, tem menos de 280 caracteres
+
+    #confere se ele e' tuitavel, ie, tem menos de 280 caracteres
     if (count_caracteres <= 280): # nao tem problema tuitar
         mais_280_caracteres = False
-    else:                          # nao eh tuitavel e por isso precisa ser dividido
+    else:                          # nao e' tuitavel e por isso precisa ser dividido
         mais_280_caracteres = True
         
         count_linhas = 0
@@ -200,16 +217,16 @@ def faz_tweet(restaurante, cardapio):
         #finalmente tuitando
         try:
             if not mais_280_caracteres: 
-                #api.update_status(tweet)
+                api.update_status(tweet)
                 print(tweet)
                 print("\nTem mais de 280 caracteres = ", mais_280_caracteres)
                 print("\nTweet publicado com sucesso!\n")
                 print("*****************************\n")
                 tweetou = True
             else:
-                #tweet = api.update_status(tweet_1)
-                #time.sleep(3) # espera um tempo para ter certeza de que o tweet foi publicado
-                #api.update_status(tweet_2, in_reply_to_status_id = tweet.id_str)
+                tweet = api.update_status(tweet_1)
+                time.sleep(3) # espera um tempo para ter certeza de que o tweet foi publicado
+                api.update_status(tweet_2, in_reply_to_status_id = tweet.id_str)
                 print(tweet_1)
                 print("\n--------outro tweet--------\n")
                 print(tweet_2)
@@ -223,8 +240,10 @@ def faz_tweet(restaurante, cardapio):
 
 
 def acessa_e_tweeta(almoco, jantar, dia_da_semana, restaurantes_almoco_seg_sex, restaurantes_almoco_sab, restaurantes_jantar_seg_sab):
+    dia_da_semana = datetime.datetime.today().weekday() # Segunda = 0, Terca = 1 ... Domingo = 6
     if almoco:
-        if dia_da_semana < 5: # se nao eh fim de semana
+
+        if dia_da_semana < 5: # se nao e' fim de semana
             for restaurante in restaurantes_almoco_seg_sex:
                 cardapio = pega_cardapio(site_fump, restaurante, almoco, jantar)
                 tweetou = faz_tweet(restaurante, cardapio)
@@ -234,7 +253,7 @@ def acessa_e_tweeta(almoco, jantar, dia_da_semana, restaurantes_almoco_seg_sex, 
                     return restaurantes_nao_tweetados
 
 
-        elif dia_da_semana == 5: # se eh sabado
+        elif dia_da_semana == 5: # se e' sabado
             for restaurante in restaurantes_almoco_sab:
                 cardapio = pega_cardapio(site_fump, restaurante, almoco, jantar)
                 tweetou = faz_tweet(restaurante, cardapio)
@@ -244,7 +263,8 @@ def acessa_e_tweeta(almoco, jantar, dia_da_semana, restaurantes_almoco_seg_sex, 
                     return restaurantes_nao_tweetados
 
     elif jantar:
-        if dia_da_semana < 6: # se nao eh domingo
+
+        if dia_da_semana != 6: # se nao e' domingo
             for restaurante in restaurantes_jantar_seg_sab:
                 cardapio = pega_cardapio(site_fump, restaurante, almoco, jantar)
                 tweetou = faz_tweet(restaurante, cardapio)
@@ -253,62 +273,46 @@ def acessa_e_tweeta(almoco, jantar, dia_da_semana, restaurantes_almoco_seg_sex, 
                     restaurantes_nao_tweetados.append(restaurante)
                     return restaurantes_nao_tweetados
 
-'''
+
+
 # definindo o horario de postagem do cardapio
-agora_almoco = datetime.datetime.now()
-horario_postagem_almoco = agora_almoco.replace(hour=9, minute=0, second=0, microsecond=0)
-agora_limite_almoco = datetime.datetime.now()
-horario_limite_postagem_almoco = agora_limite_almoco.replace(hour=9, minute=5, second=0, microsecond=0)
-
-agora_jantar = datetime.datetime.now()
-horario_postagem_jantar = agora_jantar.replace(hour=17, minute=0, second=0, microsecond=0)
-agora_limite_jantar = datetime.datetime.now()
-horario_limite_postagem_jantar = agora_limite_jantar.replace(hour=17, minute=5, second=0, microsecond=0)
-
-agora_3 = datetime.datetime.now()
-meia_noite = agora_3.replace(hour=0, minute=0, second=1, microsecond=0)
-
-
-horario_atual = datetime.datetime.now()
-
-while True:
-    
-    while horario_atual > horario_postagem_jantar: #como meia noite eh 0h, ele espera virar o dia pra comecar a comparar 
-        time.sleep(60)                              # de novo ate postar o cardapio do almoco
-        horario_atual = datetime.datetime.now() #update hora atual                                                                                                          
-    
-    while horario_atual < horario_limite_postagem_almoco: 
-        time.sleep(60) # confere, minuto a minuto a hora
-        horario_atual = datetime.datetime.now() #update hora atual
-        if horario_atual >= horario_postagem_almoco:
-            almoco = True
-            jantar = False
-            restaurantes_nao_tweetados = acessa_e_tweeta(almoco, jantar, dia_da_semana, restaurantes_almoco_seg_sex, restaurantes_almoco_sab, restaurantes_jantar_seg_sab)
-
-    while horario_atual < horario_limite_postagem_almoco: 
-        time.sleep(60) # confere, minuto a minuto a hora
-        horario_atual = datetime.datetime.now() #update hora atual
-        if horario_atual >= horario_postagem_almoco:
-            almoco = True
-            jantar = False
-            restaurantes_nao_tweetados = acessa_e_tweeta(almoco, jantar, dia_da_semana, restaurantes_almoco_seg_sex, restaurantes_almoco_sab, restaurantes_jantar_seg_sab)
-
-
-    while(horario_atual > horario_postagem_almoco and horario_atual < horario_postagem_jantar):
-        time.sleep(60)
-        horario_atual = datetime.datetime.now()
-        print(horario_atual)
-        print("Espera")
-        print("FOI!")   
+aux_postagem_almoco = datetime.datetime.now().astimezone(fuso_horario)
+horario_postagem_almoco = aux_postagem_almoco.replace(hour=9, minute=0, second=0, microsecond=0)
+'''
+aux_limite_almoco = datetime.datetime.now()
+horario_limite_postagem_almoco = aux_limite_almoco.replace(hour=9, minute=5, second=0, microsecond=0)
 '''
 
-restaurantes_nao_tweetados = acessa_e_tweeta(almoco, jantar, dia_da_semana, restaurantes_almoco_seg_sex, restaurantes_almoco_sab, restaurantes_jantar_seg_sab)
+aux_postagem_jantar = datetime.datetime.now().astimezone(fuso_horario)
+horario_postagem_jantar = aux_postagem_jantar.replace(hour=17, minute=0, second=0, microsecond=0)
+'''
+aux_limite_jantar = datetime.datetime.now()
+horario_limite_postagem_jantar = aux_limite_jantar.replace(hour=17, minute=5, second=0, microsecond=0)
+
+
+aux_meia_noite = datetime.datetime.now()
+meia_noite = aux_meia_noite.replace(hour=0, minute=0, second=0, microsecond=0)
+'''
+
+horario_atual = datetime.datetime.now().astimezone(fuso_horario)
+
+if horario_atual >= horario_postagem_almoco and horario_atual < horario_postagem_jantar:
+    almoco = True
+    jantar = False
+
+elif horario_atual >= horario_postagem_jantar:
+    almoco = False
+    jantar = True
+
+
+
+acessa_e_tweeta(almoco, jantar, dia_da_semana, restaurantes_almoco_seg_sex, restaurantes_almoco_sab, restaurantes_jantar_seg_sab)
 browser.quit() # fecha as abas abertas no navegador
 
-
-
 '''
-if len(restaurantes_nao_tweetados) > 0: # sei que essa forma de comparacao nao eh Pythonic, mas achei assim melhor fazer essa checagem de forma explicita
+restaurantes_nao_tweetados = acessa_e_tweeta(almoco, jantar, dia_da_semana, restaurantes_almoco_seg_sex, restaurantes_almoco_sab, restaurantes_jantar_seg_sab)
+
+if len(restaurantes_nao_tweetados) > 0: # sei que essa forma de comparacao nao e' Pythonic, mas achei assim melhor fazer essa checagem de forma explicita
     for restaurante in restaurantes_nao_tweetados:
         count_tentativas = 0
         tweet_falhou = "Não consegui encontrar o cardápio do {}. Tentarei novamente em 15 minutos".format(str(restaurante))
@@ -332,32 +336,4 @@ if len(restaurantes_nao_tweetados) > 0: # sei que essa forma de comparacao nao e
             elif jantar:
                 restaurantes_jantar_seg_sab = restaurantes_nao_tweetados
                 acessa_e_tweeta(almoco, jantar, restaurantes_almoco_seg_sex, restaurantes_almoco_sab, restaurantes_jantar_seg_sab)
-'''
-
-
-'''
-seconds = time.time()
-hora_local = time.ctime(seconds) 
-print(hora_local)
-
-
-agora = datetime.datetime.now()
-horario_postagem_almoco = datetime.datetime.now.replace(hour = 9, minute = 0, second = 0, microsecond = 0)
-horario_postagem_jantar = datetime.datetime.now.replace(hour = 17, minute = 0, second = 0, microsecond = 0)
-
-if (agora >= horario_postagem_almoco) and (agora < horario_postagem_jantar):
-    almoco = True
-    jantar = False
-    print("Almoco!")
-elif (agora >= horario_postagem_jantar):
-    almoco = False
-    jantar = True
-    print('Jantar!')
-
-
-#tentar_novamente = False
-#count_tentativas = 0
-
-
-#vai_bot(almoco, jantar, restaurantes_almoco_seg_sex, restaurantes_almoco_sab, restaurantes_jantar_seg_sab)
 '''
