8c81a994f94f94fe1d574422063dc14de0550119
Vitor Mafra <vitor.mafra@hotmail.com>
2020-03-25 20:58:40 -0300

    Finalizando a base da nova versao
    Bot funcional em uma nova versao modularizada e melhor documentada. Esse ainda nao eh o fim, mas um commit de atualizacao

--- environment.py
+++ environment.py
@@ -1,4 +1,5 @@
 import time
+from datetime import datetime
 
 # paginas web necessarias para buscar o cardapio
 site_fump = "http://www.fump.ufmg.br/cardapio.aspx"
@@ -12,29 +13,85 @@ keys = {
     'ACCESS_SECRET' : '',
     }
 
-# informacoes dos restaurantes
-restaurantes = {
-    'RU_SETORIAL_I' : 1,
-    'RU_SETORIAL_II' : 2,
-    'RU_SAUDE_E_DIREITO' : 3,
-    'RU_ICA' : 4,
+# informacoes relacionadas a tempo
+agora = datetime.now()
+
+if agora.hour <= 14:
+    almoco = True
+    jantar = False
+
+else:
+    almoco = False
+    jantar = True
+
+dia_da_semana = agora.weekday() # seg = 0, ter = 1, ... , dom = 6
+
+# informacoes dos restaurantes e dos cardapios
+if almoco:
+    # busca pelo cardapio do almoco, que segue a forma:
+    cardapio = {
+        'proteina_1' : "",
+        'proteina_2' : "",
+        'proteina_3' : "",
+
+        'guarnicao' : "",
+
+        'acompanhamento_1' : "",
+        'acompanhamento_2' : "",
+        'acompanhamento_3' : "",
+
+        'entrada_1' : "",
+        'entrada_2' : "",
+        'entrada_3' : "",
+
+        'sobremesa_1' : "",
+        'sobremesa_2' : "",
+
+        'refresco' : "",
+        'molho_salada' : "",
     }
 
-almoco_seg_sex = (
-    restaurantes['RU_SETORIAL_I'], 
-    restaurantes['RU_SETORIAL_II'],
-    restaurantes['RU_SAUDE_E_DIREITO'],
-    restaurantes['RU_ICA'],
-    )
+    # como eh almoco, precisa-se determinar quais sao os restaurantes do dia
+    if dia_da_semana < 5: # segunda a sexta 
+        restaurantes = {
+            'RU_SETORIAL_I' : 1,
+            'RU_SETORIAL_II' : 2,
+            'RU_SAUDE_E_DIREITO' : 3,
+            'RU_ICA' : 4,
+        }
 
-almoco_sab = (
-    restaurantes['RU_SETORIAL_I'], 
-    restaurantes['RU_SAUDE_E_DIREITO'],
-    restaurantes['RU_ICA'],
-    )
+    else: # sabado
+        restaurantes = {
+            'RU_SETORIAL_I' : 1,
+            'RU_SAUDE_E_DIREITO' : 3,
+            'RU_ICA' : 4,
+        }
 
-jantar_seg_sab = (
-    restaurantes['RU_SETORIAL_I'], 
-    restaurantes['RU_SAUDE_E_DIREITO'],
-    restaurantes['RU_ICA'],
-    )
+elif jantar:
+    # busca pelo cardapio do jantar, que esta na seguinte forma
+        cardapio = {
+            'proteina_1' : "",
+            'proteina_3' : "",
+
+            'guarnicao' : "",
+
+            'acompanhamento_1' : "",
+            'acompanhamento_2' : "",
+            'acompanhamento_3' : "",
+
+            'entrada_1' : "",
+            'entrada_2' : "",
+            'entrada_3' : "",
+
+            'sobremesa_1' : "",
+
+            'refresco' : "",
+            'molho_salada' : "",
+        }
+        
+        # no jantar, os restaurantes nao mudam de acordo com o dia da semana
+        restaurantes = {
+            'RU_SETORIAL_I' : 1,
+            'RU_SAUDE_E_DIREITO' : 3,
+            'RU_ICA' : 4,
+        }       
--- tweet.py
+++ tweet.py
@@ -3,49 +3,161 @@ import environment
 
 
 def get_keys(keys):
-        '''
-        Le em um arquivo cada uma das chaves/tokens de autenticacao fornecidos
-        pelo Twitter para publicar na conta do bot e armazana esses dados em
-        um dicionario 
-        '''
-        with open("secret_user_keys.txt", "r") as user_keys:
-                for key in environment.keys:
-                        environment.keys[key] = user_keys.readline().rstrip()
+    '''
+    Le em um arquivo cada uma das chaves/tokens de autenticacao fornecidos
+    pelo Twitter para publicar na conta do bot e armazana esses dados em
+    um dicionario 
+    '''
+    with open("secret_user_keys.txt", "r") as user_keys:
+            for key in environment.keys:
+                environment.keys[key] = str(user_keys.readline().rstrip())
 
-        user_keys.close()
-        print(environment.keys)
+            user_keys.close()
 
 
 def set_API(keys):
-        '''
-        Utiliza as chaves de autenticacao nas funcoes da biblioteca tweepy,
-        configurando assim todo o codigo necessario para publicar os tweets
-        no perfil @bot_RU_UFMG
+    '''
+    Utiliza as chaves de autenticacao nas funcoes da biblioteca tweepy,
+    configurando assim todo o codigo necessario para publicar os tweets
+    no perfil @bot_RU_UFMG
 
-        Para uma introducao facil e amigavel a essa biblioteca, veja:
-        https://tweepy.readthedocs.io/en/latest/getting_started.html#hello-tweepy
-        '''
-        auth = tweepy.OAuthHandler(str(environment.keys['CONSUMER_KEY']), str(environment.keys['CONSUMER_SECRET']))
-        auth.set_access_token(str(environment.keys['ACCESS_KEY']), str(environment.keys['ACCESS_SECRET']))
+    Para uma introducao facil e amigavel a essa biblioteca, veja:
+    https://tweepy.readthedocs.io/en/latest/getting_started.html#hello-tweepy
+    '''
+    auth = tweepy.OAuthHandler(environment.keys['CONSUMER_KEY'], \
+        environment.keys['CONSUMER_SECRET'])
 
-        api = tweepy.API(auth)
+    auth.set_access_token(environment.keys['ACCESS_KEY'], \
+        environment.keys['ACCESS_SECRET'])
 
-        return api
+    api = tweepy.API(auth)
 
+    return api
 
 
 def setup_for_tweet(keys):
-        '''
-        Funcao que chamam todas as funcoes necessarias para deixar tudo pronto
-        para a postagem dos tweets
-        '''
-        get_keys(keys)
-        return set_API(keys)
+    '''
+    Funcao que chamam todas as funcoes necessarias para deixar tudo pronto
+    para a postagem dos tweets
+    '''
+    get_keys(keys)
+    return set_API(keys)
 
 
-def faz_tweet(api, texto):
-        '''
-        Publica o tweet no perfil do bot (@bot_RU_UFMG)
-        '''
-        api.update_status(texto)
-        print("Tweet publicado com sucesso!")
+def tweeta(api, texto_tweet, mais_280_caracteres):
+    '''
+    Publica o tweet no perfil do bot (@bot_RU_UFMG)
+    '''
+    if not mais_280_caracteres:
+        api.update_status(texto_tweet)
+
+    elif mais_280_caracteres:
+        #tweet_1 = texto_tweet
+        print("erro, mais de 280 carac")
+
+    print("Tweet publicado com sucesso!")
+
+
+def elabora_tweet(restaurante, cardapio, almoco, jantar):
+    '''
+    '''
+    if restaurante == environment.restaurantes['RU_SETORIAL_I']:
+        texto_restaurante = "Cardápio RU Setorial I"
+
+    elif restaurante == environment.restaurantes['RU_SETORIAL_II']:
+        texto_restaurante = "Cardápio RU Setorial II"
+
+    elif restaurante == environment.restaurantes['RU_SAUDE_E_DIREITO']:
+        texto_restaurante = "Cardápio Saúde e Direito"
+
+    elif restaurante == environment.restaurantes['RU_ICA']:
+        texto_restaurante = "Cardápio RU ICA"
+
+    # variavel para armazenar somente a parte do cardapio que eh usada no tweet
+    cardapio_tweet = ""
+
+    # lista que determina quais partes do cardapio fazem parte do tweet
+    partes_tweetaveis = []
+
+    if almoco:
+        partes_tweetaveis = ['proteina_1', 'proteina_2','proteina_3', \
+                            'guarnicao', 'sobremesa_1', 'sobremesa_2']
+        
+        almoco_ou_jantar = "Almoço"
+
+    elif jantar:
+        partes_tweetaveis = ['proteina_1', 'proteina_3', 'guarnicao', \
+                                                        'sobremesa_1']
+        almoco_ou_jantar = "Jantar"
+
+    for elemento in partes_tweetaveis:
+        cardapio_tweet += cardapio[elemento]
+        cardapio_tweet += "\n" # pula para a proxima linha
+        if elemento == 'guarnicao':
+            cardapio_tweet += "\n" # pula mais uma linha para separar as partes
+                                   # do cardapio
+
+    tweet = texto_restaurante + " - " + almoco_ou_jantar + "\n\n"  + \
+        cardapio_tweet
+
+    return tweet
+
+
+def confere_tweet(tweet):
+    '''
+    Busca pelo texto das tags que indicam os tipos  de prato do cardapio, que
+    sempre deveriam estar presentes. Caso algum desses tipos nao seja 
+    encontrado, adiciona um emoji (⚠️) no tweet para indicar que possivelmente
+    aquele cardapio pode ter algum dos itens em falta. Depois disso, confere 
+    se o texto original tem mais de 280 caracteres (limite maximo que um tweet
+    pode ter). Retorna um booleano que indica isso
+    '''
+
+    if environment.almoco:   
+        termos_obrigatorios = ["Prato protéico 1", "Prato protéico 2", \
+            "Prato protéico 3", "Guarnição", "Sobremesa (uma porção)", \
+                 "Sobremesa 2"]
+
+    elif environment.jantar:
+        termos_obrigatorios = ["Prato protéico 1", "Prato protéico 3", \
+            "Guarnição", "Sobremesa (uma porção)"]
+
+    cardapio_completo = True
+
+    for termo_obrigatorio in termos_obrigatorios:
+        if termo_obrigatorio in tweet:
+            cardapio_completo = cardapio_completo and True
+        else:
+            # se um dos elementos nao esta no cardapio, ele ja esta errado
+            cardapio_completo = cardapio_completo and False
+
+    if not cardapio_completo:
+        print("cardapio incompleto!")
+        if environment.almoco:
+            novo_tweet = tweet[:tweet.find("Almoço") + len("Almoço")] + " ⚠️" \
+             + tweet[tweet.find("Almoço") + len("Almoço"):]
+
+        elif environment.jantar:
+            novo_tweet = tweet[:tweet.find("Jantar") + len("Jantar")] + " ⚠️" \
+             + tweet[tweet.find("Jantar") + len("Jantar"):]
+
+    else:
+        novo_tweet = None
+
+    if len(tweet) > 282:
+        mais_280_caracteres = True
+
+    else:
+        mais_280_caracteres = False
+
+    return novo_tweet, mais_280_caracteres
+
+'''
+TO DO:
+
+[] funcao que tenta buscar o cardapio de novo, caso ele nao tenha sido encontrado
+
+[] dividir o tweet em 2, caso ele tenha mais de 280 caracteres
+
+[] screenshot da pagina se o cardapio nao foi encontrado???
+'''
